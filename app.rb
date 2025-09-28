3d42d7b9f8f1e3b07170101758c84c3e7883f6e2
Drastansh Nadola <dn2286@nyu.edu>
2025-09-28 02:19:57 +0000

    Wordguesser app Part 1 working

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class WordGuesserApp < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
 
-  set :host_authorization, { permitted_hosts: [] }  
+  set :host_authorization, { permitted_hosts: [] }
 
   before do
     @game = session[:game] || WordGuesserGame.new('')
@@ -16,14 +16,19 @@ class WordGuesserApp < Sinatra::Base
     session[:game] = @game
   end
 
-  # These two routes are good examples of Sinatra syntax
-  # to help you with the rest of the assignment
   get '/' do
     redirect '/new'
   end
 
   get '/new' do
     erb :new
+  end
+
+  post '/new' do
+    # behave like /create
+    word = params[:word] || WordGuesserGame.get_random_word
+    @game = WordGuesserGame.new(word)
+    redirect '/show'
   end
 
   post '/create' do
@@ -35,32 +40,45 @@ class WordGuesserApp < Sinatra::Base
     redirect '/show'
   end
 
-  # Use existing methods in WordGuesserGame to process a guess.
-  # If a guess is repeated, set flash[:message] to "You have already used that letter."
-  # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    letter = params[:guess].to_s[0]
+
+    if letter.nil?
+      flash[:message] = "Invalid guess."
+    else
+      begin
+        already_used = @game.guesses.include?(letter.downcase) ||
+                       @game.wrong_guesses.include?(letter.downcase)
+        ok = @game.guess(letter)
+        flash[:message] = "You have already used that letter." if !ok || already_used
+      rescue ArgumentError
+        flash[:message] = "Invalid guess."
+      end
+    end
+
     redirect '/show'
   end
 
-  # Everytime a guess is made, we should eventually end up at this route.
-  # Use existing methods in WordGuesserGame to check if player has
-  # won, lost, or neither, and take the appropriate action.
-  # Notice that the show.erb template expects to use the instance variables
-  # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      @wrong_guesses     = @game.wrong_guesses
+      @word_with_guesses = @game.word_with_guesses
+      erb :show
+    end
   end
 
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    redirect '/show' unless @game.check_win_or_lose == :win
+    erb :win
   end
 
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    redirect '/show' unless @game.check_win_or_lose == :lose
+    erb :lose
   end
 end
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,23 +1,41 @@
 class WordGuesserGame
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || letter !~ /[A-Za-z]/
+    letter = letter.downcase
+
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    if @word.downcase.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    true
+  end
+
+  def word_with_guesses
+    @word.chars.map { |ch|
+      @guesses.include?(ch.downcase) ? ch : '-'
+    }.join
+  end
+
+  def check_win_or_lose
+    return :win  if word_with_guesses == @word
+    return :lose if @wrong_guesses.length >= 7
+    :play
+  end
+
   def self.get_random_word
-    require 'uri'
     require 'net/http'
     uri = URI('http://randomword.saasbook.info/RandomWord')
-    Net::HTTP.new('randomword.saasbook.info').start do |http|
-      return http.post(uri, "").body
-    end
+    Net::HTTP.post_form(uri, {}).body
   end
 end
